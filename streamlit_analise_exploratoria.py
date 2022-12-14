475909ab6696d7dd6335014b94f1b612b2a05d48
José Fernando de Oliveira Filho <57115855+fernandooliveira7@users.noreply.github.com>
2022-12-14 08:29:08 -0300

    Update streamlit_analise_exploratoria.py

--- streamlit_analise_exploratoria.py
+++ streamlit_analise_exploratoria.py
@@ -4,7 +4,10 @@ import seaborn as sb
 import matplotlib.pyplot as plt
 import streamlit as st
 
-cols = ['appid', 'name', 'release_date', 'english', 'developer', 'publisher', 'platforms', 'required_age', 'categories', 'genres', 'steamspy_tags', 'achievements', 'positive_ratings', 'negative_ratings', 'average_playtime', 'median_playtime', 'owners', 'price']
+st.set_page_config(page_title='EDA Steam Dataset', 
+                   layout="wide")
+
+cols = ['ID', 'Nome', 'Data de lançamento', 'Em inglês?', 'Dev', 'Publicador', 'Plataformas', 'Classificação indicativa', 'Categorias', 'Gêneros', 'Tags de comunidade', 'Troféus', 'Avaliações positivas', 'Avaliações negativas', 'Tempo de jogo médio', 'Tempo de jogo mediano', 'Quantidade de jogadores', 'Preço']
 dataset = pd.read_excel(io = 'steam.xlsx', engine = 'openpyxl', sheet_name= 'steam1', skiprows= 2, usecols='A:R', nrows=500, header=None, names=cols)
 #print(dataset)
 df = pd.DataFrame(dataset)
