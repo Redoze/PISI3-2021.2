7311d793f0d54f15f0851c409c61ef78be34d03c
José Francisco de Medeiros <119902584+Redoze@users.noreply.github.com>
2023-04-04 23:48:55 -0300

    Update Visão_geral.py

--- Visão_geral.py
+++ Visão_geral.py
@@ -9,7 +9,8 @@ from wordcloud import WordCloud
 import altair as alt
 from funcs import *
 
-
+#cd 'C:\Users\josef\Desktop\Projeto 3'
+#streamlit run Visão_geral.py --server.port 80
 
 st.set_page_config(
     page_title="Análise de sentimentos em avaliações de jogos na Steam",
@@ -38,7 +39,7 @@ else:
     st.caption('review_score:  1 = review positiva, review_score: -1 = review negativa, review_votes:  1 = review recomendada, review_votes:  0 = review sem recomendação ou negativada')
     st.text("")
 
-graph_options = ["Histograma dos 10 jogos com mais reviews","Histograma de sentimentos","Histograma de contagem de reviews recomendados por sentimento","Gráfico de pizza de distribuição de sentimentos"]
+graph_options = ["Histograma de sentimentos","Histograma de contagem de reviews recomendados por sentimento","Gráfico de pizza de distribuição de sentimentos"]#"Histograma dos 10 jogos com mais reviews",
 
 st.sidebar.subheader("Use o seletor para analisar todo do conjunto de dados:")
 st.text("")
@@ -46,27 +47,27 @@ selected_chart = st.sidebar.selectbox('',graph_options)
 
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::  -  ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-if selected_chart == "Histograma dos 10 jogos com mais reviews":
-    st.subheader("Histograma dos 10 jogos com mais reviews") 
-    dados_jogos = df.groupby(["app_id", "app_name", "review_score"]).agg({"review_text": "count"}).reset_index()
-    dados_jogos = dados_jogos.rename(columns={"review_text": "review_count"})
-    top10_jogos = dados_jogos.groupby(["app_id", "app_name"]).agg({"review_count": "sum"}).sort_values("review_count", ascending=False).head(10).reset_index()
-    dados_jogos = dados_jogos[dados_jogos["app_id"].isin(top10_jogos["app_id"])]
-    barras_horizontal = px.bar(dados_jogos, y="app_name", x="review_count", color="review_score", orientation="h",
-                hover_data=["review_count"], category_orders={"app_name": top10_jogos["app_name"]})
-    barras_horizontal.update_layout(
-    #title="Os 10 jogos com mais reviews",
-    xaxis_title="Número de reviews",
-    yaxis_title="Jogo",
-    legend_title="Sentimento",
-    width=1000,
-    height=600,
-    margin=dict(l=50, r=50, t=80, b=80),
-    hovermode="closest")
-    st.plotly_chart(barras_horizontal)
-    st.write("Representação gráfica dos 10 jogos com mais reviews, a sua distribuição de sentimento e o número de reviews")
+# if selected_chart == "Histograma dos 10 jogos com mais reviews":
+#     st.subheader("Histograma dos 10 jogos com mais reviews") 
+#     dados_jogos = df.groupby(["app_id", "app_name", "review_score"]).agg({"review_text": "count"}).reset_index()
+#     dados_jogos = dados_jogos.rename(columns={"review_text": "review_count"})
+#     top10_jogos = dados_jogos.groupby(["app_id", "app_name"]).agg({"review_count": "sum"}).sort_values("review_count", ascending=False).head(10).reset_index()
+#     dados_jogos = dados_jogos[dados_jogos["app_id"].isin(top10_jogos["app_id"])]
+#     barras_horizontal = px.bar(dados_jogos, y="app_name", x="review_count", color="review_score", orientation="h",
+#                 hover_data=["review_count"], category_orders={"app_name": top10_jogos["app_name"]})
+#     barras_horizontal.update_layout(
+#     #title="Os 10 jogos com mais reviews",
+#     xaxis_title="Número de reviews",
+#     yaxis_title="Jogo",
+#     legend_title="Sentimento",
+#     width=1000,
+#     height=600,
+#     margin=dict(l=50, r=50, t=80, b=80),
+#     hovermode="closest")
+#     st.plotly_chart(barras_horizontal)
+#     st.write("Representação gráfica dos 10 jogos com mais reviews, a sua distribuição de sentimento e o número de reviews")
 
-elif selected_chart == "Histograma de sentimentos":
+if selected_chart == "Histograma de sentimentos":
     st.subheader("Histograma de sentimentos")
     st.write('')
     histograma_sentimentos = alt.Chart(df).mark_bar().encode(
@@ -98,4 +99,3 @@ elif selected_chart == "Gráfico de pizza de distribuição de sentimentos":
     height=600)
     st.plotly_chart(pizza_chart)
     st.write("Representação gráfica da proporção de sentimentos positivos e negativos nas reviews")
-
