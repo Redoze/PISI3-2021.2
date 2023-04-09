bb77764e80d238c26a72ec5793cbddcf81a9b4d2
José Francisco de Medeiros <119902584+Redoze@users.noreply.github.com>
2023-04-09 01:00:06 -0300

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,16 +1,25 @@
 import streamlit as st
 import pandas as pd
-import random as rn
 
 # Load data
-@st.cache_data
-def load_csv():
-    url = "https://www.dropbox.com/s/6ba1n1kaiwv8ea3/Dataset_limpo.csv?raw=1" #Dataset_limpo.csv
-    df = pd.read_csv(url)
+@st.cache_resource
+def load_data():
+    url = "https://www.dropbox.com/s/6ba1n1kaiwv8ea3/Dataset_limpo.csv?raw=1"
+    try:
+        df = pd.read_csv(url)
+    except Exception as e:
+        error_msg = "Erro ao carregar arquivo CSV"
+        st.write(error_msg)
+        raise Exception(error_msg)
     return df
 
-@st.cache_data
+@st.cache_resource
 def load_csv2(): 
-    url = "https://www.dropbox.com/s/f7lmv645avnajkd/steam.csv?raw=1" #steam.csv
-    df_tags = pd.read_csv(url)
+    url = "https://www.dropbox.com/s/f7lmv645avnajkd/steam.csv?raw=1"
+    try:
+        df_tags = pd.read_csv(url)
+    except Exception as e:
+        error_msg = "Erro ao carregar arquivo CSV"
+        st.write(error_msg)
+        raise Exception(error_msg)
     return df_tags
