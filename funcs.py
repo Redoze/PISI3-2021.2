49cd235dc433340465db39bb5264d76ce920ddab
José Francisco de Medeiros <119902584+Redoze@users.noreply.github.com>
2023-04-09 02:03:28 -0300

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pandas as pd
+import random as rn
 
 @st.cache_resource
 def load_csv():
+    p = 0.5
     url = "https://www.dropbox.com/s/6ba1n1kaiwv8ea3/Dataset_limpo.csv?raw=1"
     try:
-        df = pd.read_csv(url)
+        df = pd.read_csv(url, skiprows=lambda i: i>0 and rn.random() > p)
     except Exception as e:
         error_msg = "Erro ao carregar arquivo CSV"
         st.write(error_msg)
