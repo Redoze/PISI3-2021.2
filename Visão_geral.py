86a0d62e21d988437e4a4e991c00ad9849fdda7c
José Fernando de Oliveira Filho <57115855+fernandooliveira7@users.noreply.github.com>
2023-07-21 11:09:39 -0300

    Update Visão_geral.py

--- Visão_geral.py
+++ Visão_geral.py
@@ -8,13 +8,13 @@ import plotly.graph_objects as go
 from funcs import *
 
 st.set_page_config(
-    page_title="AnÃ¡lise de sentimentos em avaliaÃ§Ãµes de jogos na Steam",
-    page_icon="âœ…",
+    page_title="Análise de sentimentos em avaliações de jogos na Steam",
+    page_icon="✅",
     layout="centered",
 )
 
 def build_header():
-    st.title("AnÃ¡lise de sentimentos em avaliaÃ§Ãµes de jogos na Steam")
+    st.title("Análise de sentimentos em avaliações de jogos na Steam")
     st.markdown("---")
 
 def build_body():
@@ -25,20 +25,20 @@ def build_body():
         st.sidebar.write('Conjunto de dados ocultado')
 
     else:
-        st.header('VisÃ£o geral do conjunto de dados')
+        st.header('Visão geral do conjunto de dados')
         st.text("")
         pega_df2 = carrega_df('df2')
-        pega_df2 = pega_df2.rename(columns={'app_id_df2': 'id', 'app_name_df2': 'nome', 'release_date': 'LanÃ§amento',
+        pega_df2 = pega_df2.rename(columns={'app_id_df2': 'id', 'app_name_df2': 'nome', 'release_date': 'Lançamento',
                                             'developer': 'Desenvolvedor', 'publisher': 'Publicador', 'platforms': 'Plataforma',
-                                            'required_age': 'Faixa etÃ¡ria', 'categories': 'Categorias', 'genres': 'GÃªneros',
+                                            'required_age': 'Faixa etária', 'categories': 'Categorias', 'genres': 'Gêneros',
                                             'steamspy_tags' : 'Tags da Steam', 'achievements': 'Conquistas', 
-                                            'positive_ratings': 'AvaliaÃ§Ãµes positivas', 'negative_ratings': 'AvaliaÃ§Ãµes Negativas',
-                                            'owners': 'Donos?', 'price': 'PreÃ§o'})
+                                            'positive_ratings': 'Avaliações positivas', 'negative_ratings': 'Avaliações Negativas',
+                                            'owners': 'Donos?', 'price': 'Preço'})
         st.dataframe(pega_df2)
-        st.caption('A tabela acima apresenta os dados gerais dos jogos utilizados durante o trabalho. Seus dados sÃ£o provenientes do segundo dataframe.')
+        st.caption('A tabela acima apresenta os dados gerais dos jogos utilizados durante o trabalho. Seus dados são provenientes do segundo dataframe.')
         st.text("")
 
-    st.header("EstatÃ­scas gerais dos conjuntos de dados")
+    st.header("Estatísticas gerais dos conjuntos de dados")
     st.text("")
 
     col1, col2 = st.columns(2)
@@ -54,21 +54,22 @@ def build_body():
         qtd_reviews_formatado = '{:,.0f}'.format(qtd_reviews).replace(',', '.')
 
         st.write('Quantidade total de jogos: %s' % qtd_jogos_formatado)
-        st.write('Quantidade total de avaliaÃ§Ãµes: %s' % qtd_reviews_formatado)
+        st.write('Quantidade total de avaliações: %s' % qtd_reviews_formatado)
 
     with col2: 
         merged_id_e_review = mistura_colunas('app_id', 'review_text')
 
-        # Calcula a mÃ©dia de reviews por jogo
+        # Calcula a média de reviews por jogo
         media_reviews = merged_id_e_review.groupby('app_id')['app_id'].count().mean()
 
         media_reviews_formatado = '{:.2f}'.format(round(media_reviews, 2)).replace('.', ',')
 
-        st.write('Quantidade mÃ©dia de avaliaÃ§Ãµes por jogo: %s' % media_reviews_formatado)
+        st.write('Quantidade média de avaliações por jogo: %s' % media_reviews_formatado)
     
     
     
 
 build_header()
 build_body()
+
 
