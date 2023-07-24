f3591fc9cdc8f1de26b77c74ca770feb8b597988
José Francisco de Medeiros <119902584+Redoze@users.noreply.github.com>
2023-07-24 20:27:32 -0300

    Update 4_Sobre.py
    Reformulado e ajustado

--- pages/4_Sobre.py
+++ pages/4_Sobre.py
@@ -1,32 +1,66 @@
-import pandas as pd
 import streamlit as st
-import webbrowser
+from streamlit_extras.mention import mention
 
 st.set_page_config(
     page_title="Sobre",
-    page_icon="📄",
-    # layout="centered",
+    page_icon="ℹ️",
+    layout="centered",
 )
 
-st.title('Sobre')
-st.markdown("---")
-st.header('Projeto desenvolvido para a disciplina Projeto Interdisciplinar para Sistemas de Informação III - 2022.2')
-st.subheader('Grupo GGJJLM')
-st.text("")
-st.write("Participantes:")
-st.markdown(
-"""
-- Gabriel Duarte da Silva
-- Gabriel Moreira de Lemos e Silva
-- José Fernando de Oliveira Filho
-- José Francisco de Medeiros
-- Leonardo de Sousa Araújo Alcântara
-- Marcos de Oliveira de Jesus
-""")
-st.text("")
+def build_header():
 
-url1 = 'https://docs.google.com/document/d/151L1pRvdYTNYcvONrVlpuCh6-HuasvvWEu3KfF5aM-4'
-url2 = 'https://github.com/Redoze/PISI3-2022.2'
-texto = 'Acesse também o [artigo](%s) ou o [repositório](%s).' % (url1, url2)
+    st.title('Sobre')
+    st.text("")
+    st.write('**Projeto desenvolvido para a disciplina Projeto Interdisciplinar para Sistemas de Informação III - 2022.2 do curso de Bacharelado em Sistemas de Informação (BSI).**')
+    st.write('**Grupo: GGJJLM**')
+    st.text("")
 
-st.markdown(texto, unsafe_allow_html=True)
+def build_body():
+
+    st.write("Participantes:")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        func_mention(" Gabriel Duarte da Silva", 
+                    "github", "https://github.com/gabrielduaarte")
+        func_mention(" Gabriel Moreira de Lemos e Silva", 
+                    "github", "https://github.com/GabrielTFV")
+        func_mention(" José Fernando de Oliveira Filho", 
+                    "github", "https://github.com/fernandooliveira7")
+
+    with col2:
+        func_mention(" José Francisco de Medeiros", 
+                    "github", "https://github.com/Redoze")
+        func_mention(" Leonardo de Sousa Araújo Alcântara", 
+                    "github", "https://github.com/lsaa")
+        func_mention(" Marcos de Oliveira de Jesus", 
+                    "github", "https://github.com/Markie98")
+
+    st.markdown("---")
+    st.subheader('Links')
+    st.text("")
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        func_mention(" Artigo",
+                    "📄", "https://docs.google.com/document/d/151L1pRvdYTNYcvONrVlpuCh6-HuasvvWEu3KfF5aM-4")
+
+        func_mention(" Repositório", 
+                    "github", "https://github.com/Redoze/PISI3-2022.2")
+        
+    with col2:
+        func_mention(" Dataset - 1", 
+            "📘", "https://www.kaggle.com/datasets/andrewmvd/steam-reviews")
+        func_mention(" Dataset - 2", 
+            "📙", "https://www.kaggle.com/datasets/nikdavis/steam-store-games")
+        func_mention(" Dataset - 3", 
+            "📗", "https://data.mendeley.com/datasets/ycy3sy3vj2/1")
+
+def func_mention(label, icon, url):
+    mention(
+    label= label,
+    icon= icon,
+    url= url)
+
+build_header()
+build_body()
